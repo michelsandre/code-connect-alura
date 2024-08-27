c1343c5df0e1343b278362277bb0773cb1eada89
Andre Michels de Oliveira <michelsandre@gmail.com>
2024-08-27 12:53:37 -0300

    feat: :construction: adicionado pesquisa

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,20 @@ import logger from '@/logger';
 import Link from 'next/link';
 import db from '../../prisma/db';
 
-async function getAllPosts(page) {
+async function getAllPosts(page, searchTerm) {
   try {
+    const where = {};
+
+    if (searchTerm) {
+      where.title = {
+        contains: searchTerm,
+        // mode: 'insensitive', // Only for PostgreSQL
+      };
+    }
     const perPage = 4;
     const skip = (page - 1) * perPage;
 
-    const totalItems = await db.post.count();
+    const totalItems = await db.post.count({ where });
     const totalPages = Math.ceil(totalItems / perPage);
 
     const prev = page > 1 ? page - 1 : null;
@@ -20,6 +28,7 @@ async function getAllPosts(page) {
     const posts = await db.post.findMany({
       take: perPage,
       skip,
+      where,
       orderBy: { createdAt: 'desc' },
       include: {
         author: true,
@@ -31,28 +40,28 @@ async function getAllPosts(page) {
     logger.error('Falha ao obter posts', { error });
     return { data: [], prev: null, next: null };
   }
-
-  // try {
-  //   const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
-  //   if (!response.ok) throw new Error('Falha na rede');
-  //   logger.info('Posts carregados com sucesso');
-  //   return response.json();
-  // } catch (error) {
-  //   logger.error('Ops, algo deu errado: ' + error.message);
-  //   return [];
-  // }
 }
 
 export default async function Home({ searchParams }) {
   const currentPage = parseInt(searchParams?.page || 1);
-  const { data: posts, prev, next } = await getAllPosts(currentPage);
+  const searchTerm = searchParams?.q;
+
+  const { data: posts, prev, next } = await getAllPosts(currentPage, searchTerm);
 
   return (
     <main className={styles.main}>
       {posts && posts.map((post) => <CardPost post={post} key={post.id} />)}
       <footer className={styles.footer}>
-        {prev && <Link href={`/?page=${prev}`}>&lt; Página anterior</Link>}
-        {next && <Link href={`/?page=${next}`}>Próxima página &gt;</Link>}
+        {prev && (
+          <Link href={`/?${searchTerm ? `q=${searchTerm}&` : ''}page=${prev}`}>
+            &lt; Página anterior
+          </Link>
+        )}
+        {next && (
+          <Link href={`/?${searchTerm ? `q=${searchTerm}&` : ''}page=${next}`}>
+            Próxima página &gt;
+          </Link>
+        )}
       </footer>
     </main>
   );
