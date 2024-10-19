b9e594a59b4143bf30e377797f72bb1f9d68df5c
Andre Michels de Oliveira <michelsndre@gmail.com>
2024-10-19 15:48:39 -0300

    feat: :rocket: finalizado projeto

--- src/components/Replies/index.jsx
+++ src/components/Replies/index.jsx
@@ -1,12 +1,31 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import styles from './replies.module.css';
-import { Comment } from '../Comment';
-import { ModalReply } from '../ModalReply';
+import { useEffect, useState } from "react";
+import styles from "./replies.module.css";
+import { Comment } from "../Comment";
+import { ModalReply } from "../ModalReply";
 
 export const Replies = ({ comment }) => {
   const [showReplies, setShowReplies] = useState(false);
+  const [replies, setReplies] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await fetch(`/api/comment/${comment.id}/replies`);
+
+      if (!response.ok) throw new Error("Error on fetching data");
+      const data = await response.json();
+
+      setReplies(data);
+    } catch (error) {
+      console.log("Error on fetch data", error);
+    }
+  }
+  useEffect(() => {
+    if (showReplies) {
+      fetchData();
+    }
+  }, [showReplies]);
 
   return (
     <div className={styles.container}>
@@ -15,11 +34,11 @@ export const Replies = ({ comment }) => {
           className={styles.btn}
           onClick={() => setShowReplies(!showReplies)}
         >
-          {showReplies ? 'Ocultar' : 'Ver'} respostas
+          {showReplies ? "Ocultar" : "Ver"} respostas
         </button>
         {showReplies && (
           <ul>
-            {comment.children.map((reply) => (
+            {replies.map((reply) => (
               <li key={reply.id}>
                 <Comment comment={reply} />
                 <ModalReply comment={reply} />
